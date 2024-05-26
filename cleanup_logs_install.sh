17a689696514c49ec58c099358bbba25d453754f
NyiNyiSoePaing <34880589+NyiNyiSoePaing@users.noreply.github.com>
2024-05-26 12:29:01 +0630

    Update cleanup_logs_install.sh

--- cleanup_logs_install.sh
+++ cleanup_logs_install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Download
+echo "script download"
 
 rm /home/ubuntu/cleanup_logs.sh
 wget -O /home/ubuntu/cleanup_logs.sh  https://raw.githubusercontent.com/NyiNyiSoePaing/shell_script/main/docker_cleanup_logs.sh
 
 chmod +x /home/ubuntu/cleanup_logs.sh
 
+echo "run script"
+bash /home/ubuntu/cleanup_logs.sh
+
+echo "setup cron"
 cat <(sudo crontab -l) <(echo "0 2 * * * /home/ubuntu/cleanup_logs.sh >> /home/ubuntu/cleanup_logs.log 2>&1") | sudo crontab -
 
